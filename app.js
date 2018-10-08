45b3a5db32099cccd74b1dac47f0069e6ee72f16
Sean <omerta201@yahoo.com>
2018-10-08 13:05:13 -0400

    test

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.render("home");
+
 });
 
-// app.listen(process.env.PORT, process.env.IP);
+app.listen(process.env.PORT, process.env.IP);
 
-app.listen(3000, function () {
-    console.log("Server is live on Port 3000");
-});
+// app.listen(4000, function () {
+//     console.log("Server is live on Port 3000");
+// });
