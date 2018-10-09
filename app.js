b1661ba74f7eaa97a56cc669e753c848596b5017
Sean <omerta201@yahoo.com>
2018-10-09 19:37:14 -0400

    fixed download and sass bugs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-
+    res.render('home');
 });
 
 app.listen(process.env.PORT, process.env.IP);
