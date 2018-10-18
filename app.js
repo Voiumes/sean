cf6ac1675e662fa086326764b4ffad4bafe0f95a
Sean <omerta201@yahoo.com>
2018-10-17 21:38:55 -0400

    updated resume

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.get("/", function (req, res) {
     res.render('home');
 });
 
-app.listen(process.env.PORT, process.env.IP);
+// app.listen(process.env.PORT, process.env.IP);
 
-// app.listen(3000, function () {
-//     console.log("Server is live on Port 3000");
-// });
+app.listen(3000, function () {
+    console.log("Server is live on Port 3000");
+});
